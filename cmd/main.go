f598668f2d998ea7127567d4280fc2d42cc2b62b
Arun Dhyani <dhyaniarun7@gmail.com>
2021-07-07 21:25:15 +0530

    graphQL boilerplate added

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
 	"eth2-crawler/crawler"
+	"eth2-crawler/graph"
+	"eth2-crawler/graph/generated"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
+
+const defaultPort = "8080"
 
 func main() {
 	// TODO collect config from a config files or from command args and pass to Start()
-	crawler.Start()
+	go crawler.Start()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	// TODO: make playground accessible only in Dev mode
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module eth2-crawler
 go 1.16
 
 require (
+	github.com/99designs/gqlgen v0.13.0
 	github.com/ethereum/go-ethereum v1.10.4
 	github.com/ipfs/go-cid v0.0.7 // indirect
 	github.com/ipfs/go-datastore v0.4.5 // indirect
 	github.com/libp2p/go-libp2p v0.14.3
 	github.com/libp2p/go-libp2p-core v0.8.5
 	github.com/mr-tron/base58 v1.2.0 // indirect
-	github.com/multiformats/go-multiaddr v0.3.3 // indirect
+	github.com/multiformats/go-multiaddr v0.3.3
 	github.com/multiformats/go-multihash v0.0.15 // indirect
+	github.com/vektah/gqlparser/v2 v2.1.0
 )
