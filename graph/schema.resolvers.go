c63e61b1940b2c56542926597744bed21e024e74
Sadiqur Rahman <sadiqurr8@gmail.com>
2021-10-22 13:52:24 +0600

    Fixed Altair check (#119)
    * added api for checking altair upgrade
    
    * fixed lint
    
    * added licence
    
    * fixed supportAltairUpgrade function

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -226,27 +226,27 @@ func supportAltairUpgrade(clientName, ver string) bool {
 	switch svcModels.ClientName(clientName) {
 	case svcModels.PrysmClient:
 		v, _ := version.NewVersion("v2.0.0")
-		if v.GreaterThanOrEqual(clientVersion) {
+		if clientVersion.GreaterThanOrEqual(v) {
 			return true
 		}
 	case svcModels.TekuClient:
 		v, _ := version.NewVersion("v21.9.2")
-		if v.GreaterThanOrEqual(clientVersion) {
+		if clientVersion.GreaterThanOrEqual(v) {
 			return true
 		}
 	case svcModels.LighthouseClient:
 		v, _ := version.NewVersion("v2.0.0")
-		if v.GreaterThanOrEqual(clientVersion) {
+		if clientVersion.GreaterThanOrEqual(v) {
 			return true
 		}
 	case svcModels.NimbusClient:
 		v, _ := version.NewVersion("v1.5.0")
-		if v.GreaterThanOrEqual(clientVersion) {
+		if clientVersion.GreaterThanOrEqual(v) {
 			return true
 		}
 	case svcModels.LodestarClient:
 		v, _ := version.NewVersion("v0.31.0")
-		if v.GreaterThanOrEqual(clientVersion) {
+		if clientVersion.GreaterThanOrEqual(v) {
 			return true
 		}
 	default:
